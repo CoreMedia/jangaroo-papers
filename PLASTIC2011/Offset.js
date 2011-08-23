9cd35dad2bbaacb2f843c6f53465bacd36d75f13
Andreas Gawecki <andreas.gawecki@coremedia.com>
2011-08-22 18:51:44 -0700

    version ready to upload

--- PLASTIC2011/Offset.js
+++ PLASTIC2011/Offset.js
@@ -1,24 +1,23 @@
-1 joo.classLoader.prepare("package joo.util",/* {*/
- 2  "public class Offset",1,function($$private){;return[ 
- 3    "public static const",{ HOME/* :Offset*/ :function(){return( new 
-joo.util.Offset(0, 0));}},
- 4    "public var",{ left/* :Number*/:NaN},
- 5    "public var",{ top/* :Number*/:NaN},
- 6 
- 7    "public  function Offset",function Offset$(left/* :Number*/, top/* :Number*/) {
- 8      this.left = left;
- 9      this.top = top;
-10    },
-11    "public  function clone",function clone()/* :Offset*/  {
-12      return new joo.util.Offset(this.left, this.top);
-13    },
-14    "public  function getDistance",function getDistance()/* :Number*/  {
-15      return Math.sqrt(this.left*this.left + this.top*this.top);
-16    },
-17    "public  function scale",function scale(factor/*  :Number*/)/* :Offset*/  {
-18      return new joo.util.Offset(this.left*factor, this.top*factor);
-19    },
-20    "public  function isHome",function isHome()/* :Boolean*/  {
-21      return this.left==0 && this.top==0;
-22    },
-23  ];},[],["Math"], "0.8.0", "0.8.4");
+joo.classLoader.prepare("package joo.util",/* {*/
+  "public class Offset",1,function($$private){;return[ 
+  "public static const",{ HOME/* :Offset*/ :function(){return( new joo.util.Offset(0, 0));}},
+  "public var",{ left/* :Number*/:NaN},
+  "public var",{ top/* :Number*/:NaN},
+
+  "public  function Offset",function Offset$(left/* :Number*/, top/* :Number*/) {
+    this.left = left;
+    this.top = top;
+  },
+  "public  function clone",function clone()/* :Offset*/  {
+    return new joo.util.Offset(this.left, this.top);
+  },
+  "public  function getDistance",function getDistance()/* :Number*/  {
+    return Math.sqrt(this.left*this.left + this.top*this.top);
+  },
+  "public  function scale",function scale(factor/*  :Number*/)/* :Offset*/  {
+    return new joo.util.Offset(this.left*factor, this.top*factor);
+  },
+  "public  function isHome",function isHome()/* :Boolean*/  {
+    return this.left==0 && this.top==0;
+  },
+];},[],["Math"], "0.8.0", "0.8.4");
