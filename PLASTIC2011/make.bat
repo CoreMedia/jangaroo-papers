185a53e94c3621eaae2aee4d3de7eb1c10275df9
Andreas Gawecki <andreas.gawecki@coremedia.com>
2011-12-12 16:37:09 -0800

    follow SIGPLAN file naming convention, US letter size, conference info, fixed some line breaks

--- PLASTIC2011/make.bat
+++ PLASTIC2011/make.bat
@@ -1,4 +1,6 @@
-pdflatex .\plas04-gawecki.tex
-bibtex .\plas04-gawecki.aux
-pdflatex .\plas04-gawecki.tex
-pdflatex .\plas04-gawecki.tex
+pdflatex plas04-gawecki
+bibtex plas04-gawecki.aux
+pdflatex plas04-gawecki
+pdflatex plas04-gawecki
+
+rem dvips -P pdf -t letter -o plas04-gawecki.ps plas04-gawecki.dvi
