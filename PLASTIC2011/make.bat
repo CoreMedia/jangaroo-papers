afae9b942e8f58cc52f9ec780ba1980524978089
Andreas Gawecki <andreas.gawecki@coremedia.com>
2011-12-12 20:17:32 -0800

    converted PNG to EPS for PS generation, updated make.bat for both PS and PDF generation, changed includegraphics command to compile both with latex and with pdflatex

--- PLASTIC2011/make.bat
+++ PLASTIC2011/make.bat
@@ -1,6 +1,7 @@
-pdflatex plas04-gawecki
+latex plas04-gawecki
 bibtex plas04-gawecki.aux
-pdflatex plas04-gawecki
-pdflatex plas04-gawecki
+latex plas04-gawecki
+latex plas04-gawecki
+dvips -P pdf -t letter -o plas04-gawecki.ps plas04-gawecki.dvi
 
-rem dvips -P pdf -t letter -o plas04-gawecki.ps plas04-gawecki.dvi
+rem  pdf2ps plas04-gawecki.pdf las04-gawecki.ps
